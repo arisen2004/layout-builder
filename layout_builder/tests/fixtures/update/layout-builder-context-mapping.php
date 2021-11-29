d6fdec2bc1c62bf16dcac2d9ddcb8fb1d09ca286
arisen <arisengeorge@gmail.com>
2021-11-29 18:57:04 +0530

    9 layout builder

--- layout_builder/tests/fixtures/update/layout-builder-context-mapping.php
+++ layout_builder/tests/fixtures/update/layout-builder-context-mapping.php
@@ -2,14 +2,14 @@
 
 /**
  * @file
- * Test fixture.
+ * Test context mapping update path by adding a layout without a context map.
  */
 
 use Drupal\Core\Database\Database;
 
 $connection = Database::getConnection();
 
-// Add a layout plugin with a dependency to an existing entity view display.
+// Add a layout plugin to an existing entity view display.
 $display = $connection->select('config')
   ->fields('config', ['data'])
   ->condition('collection', '')
@@ -17,17 +17,18 @@ $display = $connection->select('config')
   ->execute()
   ->fetchField();
 $display = unserialize($display);
+$display['dependencies']['module'][] = 'layout_builder';
+$display['dependencies']['module'][] = 'layout_discovery';
+$display['third_party_settings']['layout_builder']['allow_custom'] = FALSE;
+$display['third_party_settings']['layout_builder']['enabled'] = TRUE;
 $display['third_party_settings']['layout_builder']['sections'][] = [
-  'layout_id' => 'layout_test_dependencies_plugin',
-  'layout_settings' => [],
+  'layout_id' => 'layout_onecol',
+  'layout_settings' => ['label' => ''],
   'components' => [],
+  'third_party_settings' => [],
 ];
 $connection->update('config')
-  ->fields([
-    'data' => serialize($display),
-    'collection' => '',
-    'name' => 'core.entity_view_display.node.article.teaser',
-  ])
+  ->fields(['data' => serialize($display)])
   ->condition('collection', '')
   ->condition('name', 'core.entity_view_display.node.article.teaser')
   ->execute();
